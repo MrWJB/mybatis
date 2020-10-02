9bb22c37e795c38cf899bf01235f49deadae110b
wangjibin <w1812121918@163.com>
2020-10-02 22:51:04 +0800

    Initial commit

---
+++ src/main/java/org/apache/ibatis/session/AutoMappingBehavior.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2009-2012 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.session;
+
+/**
+ * Specifies if and how MyBatis should automatically map columns to fields/properties.
+ * 
+ * @author Eduardo Macarron
+ */
+public enum AutoMappingBehavior {
+
+  /**
+   * Disables auto-mapping.
+   */
+  NONE,
+
+  /**
+   * Will only auto-map results with no nested result mappings defined inside.
+   * 将只自动映射结果，没有内部定义的嵌套结果映射。
+   */
+  PARTIAL,
+
+  /**
+   * Will auto-map result mappings of any complexity (containing nested or otherwise).
+   * 将自动映射任何复杂性(包含嵌套或其他)的结果映射。
+   */
+  FULL
+}
